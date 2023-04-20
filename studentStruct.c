299d8fba00c8a93c7753978a9bf4484b7f7d2585
PedroZappa <pedrogzappa@gmail.com>
2023-04-20 21:02:08 +0100

    Minor presentation fixes

--- studentStruct.c
+++ studentStruct.c
@@ -10,16 +10,16 @@ int main() {
   int i; // Looping Variable
 
   // Take Input from User
-  printf("Enter student's information:\n");
+  printf("Enter Student's Information:\n");
   for (i = 0; i < 5; i++) {
     // Assign roll number to each student
     s[i].roll = i + 1; 
-    printf("\nFor roll number %d,\n", s[i].roll);
+    printf("\nStudent number %d,\n", s[i].roll);
     // Assign name to each student
-    printf("Enter %d first name: ", i + 1);
+    printf("Enter first name: ");
     scanf("%s", s[i].firstName);
     // Assign marks to each student
-    printf("Enter %d marks: ", i + 1);
+    printf("Enter marks: ");
     scanf("%f", &s[i].marks);
   }
 
@@ -28,7 +28,7 @@ int main() {
     printf("\nRoll number: %d\n", i + 1);
     printf("First name: ");
     puts(s[i].firstName); // 
-    printf("Marks: %.1f\n", s[i].marks);
+    printf("Marks: %.1f", s[i].marks);
     printf("\n");
   }
 
